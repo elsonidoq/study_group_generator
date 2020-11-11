ffbf7b40b04d6df2a179bded6afa5b138ab079f4
Pablo Rodriguez Zivic <pablo.rzivic@mercadolibre.com>
2020-11-10 22:33:27 -0300

    first version for the 2020 course

--- app.py
+++ app.py
@@ -11,16 +11,15 @@ app = Flask(__name__, template_folder='.')
 @app.route('/', methods=['GET', 'POST'])
 def index():
     email = request.form.get('email')
-    response = ''
     if email is not None:
         groups = load_groups()
         group = groups.get(email.lower().strip())
-        if group is None:
-            response = 'No encontre tu mail, si te habias registrado escribime'
-        else:
-            response = 'Tu grupo es: ' + ', '.join(group)
+        failed = group is None
+    else:
+        group = None
+        failed = False
 
-    return render_template('index.html', response=response)
+    return render_template('index.html', group=group, failed=failed)
 
 class Groups:
     def __init__(self):
@@ -32,9 +31,23 @@ class Groups:
             self.groups = {}
             for g in raw_groups:
                 for m in g:
-                    self.groups[m.lower().strip()] = g
+                    self.groups[m['Username'].lower().strip()] = g
 
         return self.groups
+
+def _row(content, is_header):
+    elem = 'th' if is_header else 'td'
+    content = ' '.join(f'<{elem}>{c}</{elem}>' for c in content)
+    return f'<tr>{content}</tr>'
+
+def format_group(group):
+    keys = 'Username coding_skills ml_skills'.split()
+
+    lines = [_row(keys, True)]
+    for doc in group:
+        lines.append(_row([doc[k] for k in keys], False))
+
+    return '<table>' + '\n'.join(lines) + '</table>'
 
 load_groups = Groups()
 
